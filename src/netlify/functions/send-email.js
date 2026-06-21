d431e943da332cacecc8bfff11da3e4d0498895a
Deviate7436 <cantorchilds@tbiport.org>
2026-06-21 18:07:21 -0400

    Update send-email.js

--- src/netlify/functions/send-email.js
+++ src/netlify/functions/send-email.js
@@ -26,14 +26,14 @@ exports.handler = async (event) => {
     const response = await fetch("https://api.resend.com/emails", {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${process.env.RESEND_API_KEY}`,
+        Authorization: "Bearer " + process.env.RESEND_API_KEY,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
         from: "TBI Progress Tracker <assignments@tbiprogresstracker.org>",
-        to,
-        subject,
-        html,
+        to: to,
+        subject: subject,
+        html: html,
         reply_to: replyTo || "assignments@tbiprogresstracker.org",
       }),
     });
